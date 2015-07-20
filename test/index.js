133f810829a49f71949a071cc4d46f2636695726
Andrew Shaffer <darawk@gmail.com>
2015-07-20 10:05:10 -0700

    fix undefined styles being added when props not present

--- test/index.js
+++ test/index.js
@@ -30,6 +30,14 @@ describe('layout', function () {
     assert.ok(cellStyle.indexOf('max-width:33.33%') !== -1)
     assert.ok(cellStyle.indexOf('flex:0 0 33.33%') !== -1)
   })
+
+  it('should not put undefind when a prop isnt specified', function() {
+    create(<Row layoutAlign='space-around'><RowCell flex='33'>test</RowCell></Row>)
+    const row = container.children[0]
+    const rowStyle = row.attributes.style.value
+
+    assert.ok(rowStyle.indexOf('align-items') === -1)
+  })
 })
 
 function create (component) {
